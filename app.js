1336fc2c4cb7a1cc829f3f1ad768f290bc804fac
thevasanthram <thevasanthram@github.com>
2022-12-26 12:07:29 +0530

    JWT implemented with NodeJS

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+app.get('/api', function (req, res) {
+  res.json({
+    text: 'my api!',
+  });
+});
+
+app.post('/api/login', function (req, res) {
+  const user = { id: 3 };
+  const token = jwt.sign({ user }, 'my_secret_key');
+  res.json({
+    token: token,
+  });
+});
+
+app.get('/api/protected', ensureToken, function (req, res) {
+  jwt.verify(req.token, 'my_secret_key', function (err, data) {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        text: 'this is protected',
+        data: data,
+      });
+    }
+  });
+});
+
+function ensureToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  console.log('bearerHeader: ', bearerHeader);
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000');
+});
